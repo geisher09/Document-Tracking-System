7535bd2c67e14b73a56851fc51e204f8bfa2b236
AnchetaChristian <cancheta29@gmail.com>
2017-09-30 15:43:30 +0800

    offices & employees with login/sgnup

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,22 +10,28 @@ class Home extends CI_Controller {
 
 
 	public function index(){
-		$header_data['title']="Login";	
-		$this->load->view('header2',$header_data);	
+		$header_data['title']="Login";
+		$this->load->view('header2',$header_data);
 		$this->load->view('login');
 	}
-	
+
 	public function home(){
-		$header_data['title']="DTS";		
+		$header_data['title']="DTS";
+		date_default_timezone_set('Asia/Manila');
+		//$time =date("g:i a");// time());
+		$time =date("h:i:sa");// time());
+		$date = date("Y-m-d");
+		$data['time'] = $time;
+		$data['date'] = $date;
 		$this->load->view('header2',$header_data);
 		$this->load->view('header');
-		$this->load->view('home');
+		$this->load->view('home',$data);
 		$this->load->view('footer');
 	}
-	
+
 	public function docu(){
 		$header_data['title']="All Documents";
-		
+
 		$this->load->view('header2',$header_data);
 		$this->load->view('header');
 		$this->load->model('dts_model');
@@ -33,89 +39,143 @@ class Home extends CI_Controller {
 		$this->load->view('documents',['do'=>$documents]);
 		$this->load->view('footer');
 	}
-	
+
 	public function add(){
 		$header_data['title']="Add Documents";
-		
+
 		$this->load->view('header2',$header_data);
 		$this->load->view('header');
 		$this->load->view('add');
 		$this->load->view('footer');
 	}
-	
+
 	public function profile(){
 		$header_data['title']="Profile";
-		
+
 		$this->load->view('header2',$header_data);
 		$this->load->view('header');
 		$this->load->view('profile');
 		$this->load->view('footer');
 	}
-	
+
 	public function contacts(){
 		$header_data['title']="Contacts";
-		
-		
-		
+
+
+
 		$this->load->view('header2',$header_data);
 		$this->load->view('header');
 		$this->load->view('contacts');
 		$this->load->view('footer');
 	}
-	
+
 	public function myd(){
 		$header_data['title']="My Documents";
-		
+
 		$this->load->view('header2',$header_data);
 		$this->load->view('header');
 		$this->load->view('mydocu');
 		$this->load->view('footer');
 	}
-	
+
 	public function edit(){
 		$header_data['title']="Edit Profile";
-		
+
 		$this->load->view('header2',$header_data);
 		$this->load->view('header');
 		$this->load->view('edit');
 		$this->load->view('footer');
 	}
-	
-	public function offices(){
-		$header_data['title']="Offices";
-		
-		$this->load->view('header2',$header_data);
-		$this->load->view('header');
-		$this->load->view('offices');
-		$this->load->view('footer');
-	}
-	
-	public function dept(){
-		$header_data['title']="Departments";
-		
-		$this->load->view('header2',$header_data);
-		$this->load->view('header');
-		$this->load->view('departments');
-		$this->load->view('footer');
-	}
-	
-	public function addDept(){
-	$header_data['title']="Add Department";
-		
-	$this->load->view('header2',$header_data);
-	$this->load->view('header');
-	$this->load->view('addDept');
-	$this->load->view('footer');
-	}
-	
+
 	public function signup(){
-		$header_data['title']="Sign Up";	
-		$this->load->view('header2',$header_data);	
+		$header_data['title']="Sign Up";
+		$this->load->view('header2',$header_data);
 		$this->load->model('dts_model');
 		$lastEmp = $this->dts_model->getLastEmployee();
 		$departments = $this->dts_model->getDepartments();
 		$this->load->view('signup', ['dp'=>$departments,'le'=>$lastEmp]);
 
+	}
+
+	//OFFICES & EMPLOYEES
+		public function offices(){
+			$header_data['title']="Offices";
+			$this->load->model('dts_model');
+			$hold = $this->dts_model->getOffices();
+			$data['offices'] = $hold;
+			//print_r($data);
+
+			$this->load->view('header2',$header_data);
+			$this->load->view('header');
+			$this->load->view('offices',$data);
+			$this->load->view('footer');
+		}
+
+		public function dept($office_id){
+			$header_data['title']="Departments";
+			$condition = array('office_id'=>$office_id);
+			$this->load->model('dts_model');
+			$hold = $this->dts_model->getDepartment2($condition);
+			$data['dept'] = $hold;
+			$data['office'] = $condition;
+			// print_r($data);
+			// print_r($office_id);
+			// echo "Pasok";
+			$this->load->view('header2',$header_data);
+			$this->load->view('header');
+			$this->load->view('departments',$data);
+			$this->load->view('footer');
+		}
+
+		public function deptEmployees($department_id){
+			$header_data['title']="Department's Employees";
+			$condition = array('department_id'=>$department_id);
+			$this->load->model('dts_model');
+			$hold = $this->dts_model->getEmployee($condition);
+			$data['employees'] = $hold;
+			$data['title'] = "None";
+			//print_r($data);
+			// echo "Pasok";
+			$this->load->view('header2',$header_data);
+			$this->load->view('header');
+			$this->load->view('department/employee',$data);
+			$this->load->view('footer');
+		}
+
+		public function addDept($office_id){
+		$header_data['title']="Add Department";
+		$data['val'] = $office_id;
+	//	print_r($data);
+		$this->load->view('header2',$header_data);
+		$this->load->view('header');
+		$this->load->view('addDept',$data);
+		$this->load->view('footer');
+		}
+		public function saveDept($val){
+		$this->form_validation->set_rules('department_id', 'Department ID', 'required');
+	 	$this->form_validation->set_rules('department_desc', 'Department Name', 'required');
+  		$this->form_validation->set_error_delimiters('<div class="text-danger bg-danger">', '</div>');
+$data['office_id'] = $val;
+//print_r($data);
+        if ($this->form_validation->run()){
+
+               	$data = $this->input->post();
+             	$this->load->model('dts_model');
+             	if ($this->dts_model->saveDepartment($data)){
+             		$this->session->set_flashdata('response', 'Saved Succesfully!');
+				 }
+				 else{
+             		$this->session->set_flashdata('response', 'Failed :(');
+				 }
+					return redirect('home/offices');
+        }
+        else{
+        $header_data['title']="Add Documents";
+				$this->load->view('header2',$header_data);
+				$this->load->view('header');
+				$this->load->view('saveDept');
+				$this->load->view('footer');
+        }
 	}
 
 	public function save(){
@@ -139,7 +199,7 @@ class Home extends CI_Controller {
 
         }
         else{
-            	$header_data['title']="Add Documents";		
+            	$header_data['title']="Add Documents";
 				$this->load->view('header',$header_data);
 				$this->load->view('add');
 				$this->load->view('footer');
@@ -156,14 +216,14 @@ class Home extends CI_Controller {
 						if(move_uploaded_file($_FILES["file"]["tmp_name"],$url))
 								return $url;
 
-		return redirect('home/add');					
+		return redirect('home/add');
 	}
-		
+
 	public function validation(){
 		$this->load->model('dts_model');
 		$query=$this->dts_model->validate();
 		// echo $query;
-		$uname['uname'] = $this->input->post('uname');	
+		$uname['uname'] = $this->input->post('uname');
 		if($query){
 			$data = array(
 				'username' => $uname,
@@ -171,13 +231,13 @@ class Home extends CI_Controller {
 			);
 			$this->session->set_userdata($data);
 			$header_data['title']="DTS";
-			$uname['uname'] = $this->input->post('uname');		
+			$uname['uname'] = $this->input->post('uname');
 			$this->load->view('header2',$header_data);
 			$this->load->view('header');
-			$this->load->view('home',$uname);			
+			$this->load->view('home',$uname);
 			$this->load->view('footer');
-			
-		} 
+
+		}
 		else{
 
 			$this->index();
@@ -200,19 +260,19 @@ class Home extends CI_Controller {
              	if ($query=$this->dts_model->saveMember()){
              		$data['account_created'] = 'Account Created!';
 
-             		$header_data['title']="Login";	
-					$this->load->view('header2',$header_data);	
+             		$header_data['title']="Login";
+					$this->load->view('header2',$header_data);
 					$this->load->view('login',$data);
 				 }
 				 else{
              		$data['account_created'] = 'Failed to Create Account!';
 				 }
-				 
+
 
         }
         else{
-            	$header_data['title']="Sign Up";	
-				$this->load->view('header2',$header_data);	
+            	$header_data['title']="Sign Up";
+				$this->load->view('header2',$header_data);
 				$this->load->model('dts_model');
 				$departments = $this->dts_model->getDepartments();
 				$this->load->view('signup', ['dp'=>$departments]);
@@ -229,33 +289,6 @@ class Home extends CI_Controller {
 		}else {
 			return FALSE;
 		}
-	}
-
-
-	public function saveDept(){
-		$this->form_validation->set_rules('department_id', 'Department ID', 'required');
-	 	$this->form_validation->set_rules('department_desc', 'Department Name', 'required');
-  		$this->form_validation->set_error_delimiters('<div class="text-danger bg-danger">', '</div>');
-
-        if ($this->form_validation->run()){
-
-               	$data = $this->input->post();
-             	$this->load->model('dts_model');
-             	if ($this->dts_model->saveDepartment($data)){
-             		$this->session->set_flashdata('response', 'Saved Succesfully!');
-				 }
-				 else{
-             		$this->session->set_flashdata('response', 'Failed :(');
-				 }
-				 return redirect('home/addDept');
-
-        }
-        else{
-            	$header_data['title']="Add Documents";		
-				$this->load->view('header',$header_data);
-				$this->load->view('addDept');
-				$this->load->view('footer');
-        }
 	}
 
 
