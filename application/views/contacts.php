d841716030483a800702a24d2a687807ab9d7cff
AngeloJose <angelojoseaberilla@gmail.com>
2017-10-15 14:55:58 +0800

    Added dummy data to contacts

--- application/views/contacts.php
+++ application/views/contacts.php
@@ -22,74 +22,189 @@
 						</thead>
 						<tbody >
 						<tr>
-							<td><li>College of Architecture and Fine Arts (CAFA)-Dean's Office</td>
-							<td>09</td>
-							<td>134.8723</td>
-							<td>cafa@gmail.com</td>
-						</tr>
-						<tr>
-							<td><li>College of Industrial Education (CIE)-Dean's Office</td>
-							<td>54</td>
-							<td>345.9876</td>
-							<td>cie@gmail.com</td></tr>
-						</tr>
-						<tr>
-							<td><li>College of Industrial Technology (CIT)-Dean's Office </td>
-							<td>097</td>
-							<td>231.6789</td>
-							<td>cit@gmail.com</td>
-						</tr>
-						<tr>
-							<td><li>College of Liberal Arts (CLA)-Dean's Office</td>
-							<td>88</td>
-							<td>234.3245</td>
-							<td>cla@gmail.com</td>
-						</tr>
-						<tr>
-							<td><li>College of Science (COS)</td><td></td><td></td><td></td>
-							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Math Department</ul></td>
-											<td>18</td>
-								<td>918.2017</td>
-								<td>cos@gmail.com</td>
+						<td><li>College of Architecture and Fine Arts (CAFA)</td>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dean's Office</ul></td>
+								<td>01</td>
+								<td>101.9145</td>
+								<td>cafadean@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Architecture Department</ul></td>
+											<td>02</td>
+								<td>102.2017</td>
+								<td>cafaarchi@gmail.com</td></tr>
 
-							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Physics Department</ul></td>
-											<td>09</td>
-								<td>918.1992</td>
-								<td>cos@gmail.com</td>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fine Arts Department</ul></td>
+											<td>03</td>
+								<td>103.1992</td>
+								<td>cafafa@gmail.com</td></tr>
 
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Graphics Technology Department</ul></td>
+								<td>04</td>
+								<td>104.1945</td>
+								<td>cafagt@gmail.com</td></tr>
+						</tr>
+						<tr>
+							<td><li>College of Industrial Education (CIE)</td>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dean's Office</ul></td>
+								<td>11</td>
+								<td>201.9145</td>
+								<td>ciedean@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Technical Arts Department</ul></td>
+											<td>12</td>
+								<td>202.2017</td>
+								<td>cietecharts@gmail.com</td></tr>
+
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home Economics Department</ul></td>
+											<td>13</td>
+								<td>203.1992</td>
+								<td>ciehe@gmail.com</td></tr>
+
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Professional Industrial Education Department</ul></td>
+								<td>14</td>
+								<td>204.1945</td>
+								<td>ciepie@gmail.com</td></tr>
+						</tr>
+						<tr>
+							<td><li>College of Industrial Technology (CIT)</td>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dean's Office</ul></td>
+								<td>21</td>
+								<td>301.9145</td>
+								<td>citdean@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Basic Industrial Technology Department</ul></td>
+											<td>22</td>
+								<td>302.2017</td>
+								<td>citbit@gmail.com</td></tr>
+
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Civil Engineering Technology Department</ul></td>
+											<td>23</td>
+								<td>303.1992</td>
+								<td>citcet@gmail.com</td></tr>
+
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Electrical Engineering Technology Department</ul></td>
+								<td>24</td>
+								<td>304.1945</td>
+								<td>citelectricalet@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Electronics Engineering Technology Department</ul></td>
+								<td>25</td>
+								<td>305.9145</td>
+								<td>citelectronicset@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Food and Apparel Technology Department</ul></td>
+											<td>26</td>
+								<td>306.2017</td>
+								<td>citfat@gmail.com</td></tr>
+
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Graphic Arts and Printing Technology Department</ul></td>
+											<td>27</td>
+								<td>307.1992</td>
+								<td>citgapt@gmail.com</td></tr>
+
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mechanical Engineering Technology Department</ul></td>
+								<td>27</td>
+								<td>308.1945</td>
+								<td>citmet@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Power Plant Engineering Technology Department</ul></td>
+								<td>28</td>
+								<td>309.1945</td>
+								<td>citppet@gmail.com</td></tr>
+						</tr>
+						<tr>
+							<td><li>College of Liberal Arts (CLA)</td>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dean's Office</ul></td>
+								<td>31</td>
+								<td>401.1945</td>
+								<td>cladean@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;English Department</ul></td>
+								<td>32</td>
+								<td>402.1945</td>
+								<td>claeng@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Filipino Department</ul></td>
+								<td>33</td>
+								<td>403.1945</td>
+								<td>clafil@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Physical Education Department</ul></td>
+								<td>34</td>
+								<td>404.1945</td>
+								<td>clape@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Social Sciences Department</ul></td>
+								<td>35</td>
+								<td>405.1945</td>
+								<td>class@gmail.com</td></tr>
+						</tr>								
+						<tr>
+							<td><li>College of Science (COS)</td>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dean's Office</ul></td>
+								<td>51</td>
+								<td>601.1945</td>
+								<td>cosdean@gmail.com</td></tr>
 							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chemistry Department</ul></td>
-								<td>17</td>
-								<td>918.1945</td>
-								<td>cos@gmail.com</td>
+								<td>52</td>
+								<td>602.1945</td>
+								<td>coschem@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mathematics Department</ul></td>
+								<td>53</td>
+								<td>603.1945</td>
+								<td>cosmath@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Physics Department</ul></td>
+								<td>54</td>
+								<td>604.1945</td>
+								<td>cosphy@gmail.com</td></tr>
+						</tr>								
+						
+						<tr>
+							<td><li>Commission on Audit (COA)</td>
+							<td>60</td>
+							<td>701.2017</td>	
+							<td>irtc@gmail.com</td>
+						</tr>
+						
+						<tr>
+							<td><li>College of Engineering (COE)</td>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dean's Office</ul></td>
+								<td>41</td>
+								<td>501.1945</td>
+								<td>coedean@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;COE Building Faculty Room</ul></td>
+								<td>42</td>
+								<td>502.1945</td>
+								<td>coefac@gmail.com</td></tr>
+							<tr><td><ul>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;COE ESEP Building</ul></td>
+								<td>43</td>
+								<td>503.1945</td>
+								<td>coeesep@gmail.com</td></tr>
 						</tr>
 						<tr>
 							<td><li>Integrated Research and Training Center (IRTC)</td>
-							<td>410</td>
-							<td>917.2017</td>	
+							<td>61</td>
+							<td>702.2017</td>	
 							<td>irtc@gmail.com</td>
 						</tr>
 						<tr>
-							<td><li>Office of the University Research and Development Services (URDS)</td>
-							<td>431</td>
-							<td>251.2917</td>
-							<td>--</td>
+							<td><li>Office of the President</td>
+							<td>62</td>
+							<td>703.2017</td>	
+							<td>op@gmail.com</td>
 						</tr>
 						<tr>
 							<td><li>Office of the Student Affairs (OSA)</td>
-							<td>31</td>
-							<td>123.4567</td>
+							<td>63</td>
+							<td>704.4567</td>
 							<td>--</td>	
 						</tr>
 						<tr>
+							<td><li>Office of the University Research and Development Services (URDS)</td>
+							<td>64</td>
+							<td>705.2917</td>
+							<td>--</td>
+						</tr>
+						<tr>
 							<td><li>Office of the Vice President for Academic Affairs</td>
-							<td>564</td>
-							<td>981.2707</td>
+							<td>65</td>
+							<td>706.2707</td>
 							<td>vpaa@gmail.com</td>
 						</tr>
 						<tr>
 							<td><li>Office of the Vice President for Research and Extension</td>
-							<td>231</td>
-							<td>931.2717</td>
+							<td>66</td>
+							<td>707.2717</td>
 							<td>vpre@gmail.com</td>
 						</tr>
 						
