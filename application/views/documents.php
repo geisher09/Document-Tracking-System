a2e8d2aaf657a4a87284511453baa6da2a95c05b
abendaniocarlo <carlocabendanio@gmail.com>
2017-10-22 18:55:49 +0800

    slight changes on views

--- application/views/documents.php
+++ application/views/documents.php
@@ -544,7 +544,7 @@ function lol(id){
 			<div class="clearfix"></div>
 			<footer>
 			<div class="container-fluid">
-				<p class="copyright">&copy; 2017 <a href="https://www.themeineed.com" target="_blank">Document Tracki</a>. All Rights Reserved.</p>
+				<p class="copyright">&copy; 2017 <a href="<?php echo site_url('Home/home'); ?>" target="_blank">Document Tracking System</a>. All Rights Reserved.</p>
 			</div>
 			</footer>
 
